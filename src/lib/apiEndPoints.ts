013dc56114e47cd8aa27678bbf151c84210f7175
Lyquang <lythanhnhatquangthongnhat2004@gmail.com>
2025-12-24 11:49:38 +0700

    fix data to check vercel

--- src/lib/apiEndPoints.ts
+++ src/lib/apiEndPoints.ts
@@ -1,10 +1,14 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
+// https://bkment-identity-service-latest.onrender.com/identity/auth/login
 
+const IDENTITY_URL = 'https://bkment-identity-service-latest.onrender.com/identity';
+// const ARTICLE_URL = process.env.NEXT_PUBLIC_ARTICLE_API_URL || '';
+// const USER_URL = process.env.NEXT_PUBLIC_USER_API_URL || '';
 export const API_ENDPOINTS = {
     AUTH: {
-        LOGIN: `${API_BASE_URL}/api/auth/login`,
-        REGISTER: `${API_BASE_URL}/api/auth/register`,
-        PROFILE: `${API_BASE_URL}/api/auth/me`,
+        LOGIN: `${IDENTITY_URL}/api/auth/login`,
+        REGISTER: `${IDENTITY_URL}/api/auth/register`,
+        PROFILE: `${IDENTITY_URL}/api/auth/me`,
     },
     ARTICLES: {
         GET_ALL: `${API_BASE_URL}/api/articles`,
@@ -18,3 +22,4 @@ export const API_ENDPOINTS = {
         LIST: `${API_BASE_URL}/api/users`,
     }
 };
+
